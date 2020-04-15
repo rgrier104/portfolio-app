9a980791c4b3072c6953bcf14ea3f90bc575ce22
rgrier104 <rgrier104@gmail.com>
2020-04-14 17:00:35 -1000

    add navigation component with navbar route links using react-router-dom

---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Rachel Grier</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+                <Nav className="ml-auto">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Navigation;
