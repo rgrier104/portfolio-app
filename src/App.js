cf6d703b53e34f6d0ce2f3a9304a3bfbfc501746
rgrier104 <rgrier104@gmail.com>
2020-04-21 18:22:33 -1000

    add routes to app for home and projects paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Projects from './components/Projects';
 import Navigation from './components/Navigation';
 import './App.css';
 
@@ -10,7 +11,8 @@ function App() {
     <Router>
       <Container fluid>
         <Navigation />
-        <Home />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/" component={Home} />
       </Container>
     </Router>
     
